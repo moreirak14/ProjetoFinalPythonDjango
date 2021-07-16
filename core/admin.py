937734efb37cce7325988a65beb6b82a3f067f5f
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-15 22:39:57 -0300

    Criação da classe MovRotativos
    Classe MovRotativos retorna data entrada, data saida, valor por hora, veiculo desejado e se foi pago ou não.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pessoa, Marca, Veiculo, Parametros
+from .models import Pessoa, Marca, Veiculo, Parametros, MovRotativo
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Pessoa)
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Parametros)
+admin.site.register(MovRotativo)
