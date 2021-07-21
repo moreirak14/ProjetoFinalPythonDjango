ffd37f21b615c92b61bd484050fa82933d08f85b
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-21 18:51:22 -0300

    Criação do arquivo urls.py core e uma nova view

--- core/urls.py
+++ core/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
-    path('core/', include('core.urls')),
-    path('admin/', admin.site.urls),
+    path('', home, name='core.home'),
+
 ]
