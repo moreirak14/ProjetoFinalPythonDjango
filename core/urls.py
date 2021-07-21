a8e14bb3fa000777a6e16f3f1d9431acc1dc215f
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-21 19:15:05 -0300

    Criação da view lista_pessoas e listagem de pessoas no .html

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from .views import home
+from .views import home, lista_pessoas
 
 urlpatterns = [
     path('', home, name='core.home'),
+    path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 
 def home(request):
     context = {'mensagem': 'Olá Mundo'}
     return render(request, 'core/index.html', context)
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
