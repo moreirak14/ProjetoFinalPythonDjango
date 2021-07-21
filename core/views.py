0fe54c246c8284397d0febb697203f31ce6fffae
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-21 19:03:22 -0300

    Primeiro template core/index.html criado

--- core/views.py
+++ core/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Olá')
+    context = {'mensagem': 'Olá Mundo'}
+    return render(request, 'core/index.html', context)
