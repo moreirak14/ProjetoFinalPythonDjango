eb7f5b72ad6b75e82ff9a0a9e71b088a64d645a5
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-21 18:41:40 -0300

    Criação da classe MovMensalista

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from .models import Pessoa, Marca, Veiculo, Parametros, MovRotativo, Mensalista
+from .models import Pessoa, Marca, Veiculo, Parametros, MovRotativo, Mensalista, MovMensalista
 
 # Register your models here.
 
 class MovRotativoAdmin(admin.ModelAdmin):
     list_display = ('checkin', 'checkout', 'veiculo','valor_hora', 'horas_total','total', 'pago')
 
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ('mensalista', 'dt_pgto', 'total')
+
 admin.site.register(Pessoa)
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Parametros)
 admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
 admin.site.register(MovRotativo, MovRotativoAdmin)
