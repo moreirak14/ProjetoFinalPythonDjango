e6cb16e9e5fb9ade424a391937618a9e567c5dbe
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-07-21 19:34:07 -0300

    Criação da view lista_veiculos e lista_movrotativos

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from .views import home, lista_pessoas
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos,
+)
 
 urlpatterns = [
     path('', home, name='core.home'),
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('mov-rot-list/', lista_movrotativos, name='core_lista_movrotativos'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovRotativo
+)
 
 # Create your views here.
 
@@ -7,6 +11,17 @@ def home(request):
     context = {'mensagem': 'Olá Mundo'}
     return render(request, 'core/index.html', context)
 
+
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_movrotativos(request):
+    mov_rotativos = MovRotativo.objects.all()
+    return render(request, 'core/lista_movrotativos.html', {'mov_rotativos': mov_rotativos})
